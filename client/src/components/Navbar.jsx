e280752ff9c5cb4ab9a6cdc42e50ae300db8c2fe
PrashanthSai-K <114280375+PrashanthSai-K@users.noreply.github.com>
2023-05-19 21:56:36 +0530

    Navbar animation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/navbar.css";
 
 const Navbar = () => {
 
+  const [scrolled, setScrolled] = useState(false);
 
-  if(window.pageYOffset > 10 ){
-    document.getElementById("hidden-navbar").classList.add("navbar-container-2")
-  }
+  const handlescroll = () => {
 
+    const scrolldistance = window.scrollY;
+
+    if (scrolldistance > 200) {
+      setScrolled(true);
+    }else{
+      setScrolled(false);
+    }
+  };
+
+  window.addEventListener("scroll", handlescroll);
 
   return (
     <>
-      <div className="navbar-container " >
+      <div
+        className={
+          scrolled ? "navbar-container navbar-scrolled" : "navbar-container"
+        }
+      >
         <div className="nav-name">Sai Prashanth K</div>
         <nav className="navlinks">
           <a href="#home">Home</a>
@@ -22,7 +35,6 @@ const Navbar = () => {
         </nav>
         <div className="nav-icons">Github</div>
       </div>
-
     </>
   );
 };
